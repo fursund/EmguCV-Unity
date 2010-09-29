78869023a58fc868c6261ed38fcab8520312c90e
Jens Fursund <jens@fursund.com>
2010-09-29 11:02:48 +0200

    add some missing files

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyVersion("2.1.0.793")]
+[assembly: AssemblyFileVersion("2.1.0.793")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Emgu")]
+[assembly: AssemblyProduct("Emgu.CV")]
+[assembly: AssemblyCopyright("Copyright � Emgu 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
---
+++ Emgu.CV/PInvoke/CvInvokeEntryPoints.cs
@@ -0,0 +1,29 @@
+//This file is automatically generate by CMAKE. DO NOT MODIFY.
+using System;
+
+namespace Emgu.CV
+{
+   public partial class CvInvoke
+   {
+      /// <summary>
+      /// The file name of the cxcore library
+      /// </summary>
+      public const string CXCORE_LIBRARY = "cxcore210";
+      /// <summary>
+      /// The file name of the cv library
+      /// </summary>
+      public const string CV_LIBRARY = "cv210";
+      /// <summary>
+      /// The file name of the highgui library
+      /// </summary>
+      public const string HIGHGUI_LIBRARY = "highgui210";
+      /// <summary>
+      /// The file name of the cvaux library
+      /// </summary>
+      public const string CVAUX_LIBRARY = "cvaux210";
+      /// <summary>
+      /// The file name of the cvextern library
+      /// </summary>
+      public const string EXTERN_LIBRARY = "cvextern210";
+   }
+}
